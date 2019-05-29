4690c509d095551d38cf1fb0a1bf97d685b42c82
Justin Brown <justinbrown.work@gmail.com>
2019-05-28 23:06:47 -0400

    Fetching and displaying a basic list of list names for now.

---
+++ src/features/List/Sidebar/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { filter } from "graphql-anywhere";
+import { Text, View } from "react-native";
+import { ListSidebarItem } from "./Item";
+
+export const ListSidebar = () => {
+  return (
+    <Query
+      query={sidebarQuery}
+      fetchPolicy="network-only"
+      variables={{ ...ListSidebar.sidebarQueryDefaultVariables }}
+    >
+      {({ data, loading, error, fetchMore }) => {
+        if (error) {
+          return <Text>Error: {error}</Text>;
+        }
+
+        if (loading && !data.listLists) {
+          return <Text>Loading...</Text>;
+        }
+
+        return (
+          <View>
+            {data.listLists.items.map(item => (
+              <ListSidebarItem
+                key={item.id}
+                {...filter(ListSidebarItem.fragment, item)}
+              />
+            ))}
+          </View>
+        );
+      }}
+    </Query>
+  );
+};
+
+ListSidebar.fragment = gql`
+  fragment ListSidebarFragment on ModelListConnection {
+    items {
+      ...ListSidebarItemFragment
+    }
+    nextToken
+  }
+  ${ListSidebarItem.fragment}
+`;
+
+ListSidebar.sidebarQueryDefaultVariables = {
+  limit: 40
+};
+
+export const sidebarQuery = gql`
+  query ListLists(
+    $filter: ModelListFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listLists(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      ...ListSidebarFragment
+    }
+  }
+  ${ListSidebar.fragment}
+`;
